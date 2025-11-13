ac4f97ee4f8b2eeb71bf33b6d96d6976603b6b55
Cuongdevcode <kennypham19042003@gmail.com>
2025-11-13 11:25:42 +0700

    Code FE PRN232

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🎉 TailwindCSS chạy thành công!
+      </h1>
+      <p className="text-gray-600 mt-2">
+        Đây là giao diện từ Tailwind.
+      </p>
+    </div>
+  );
+}
+
+export default App;
